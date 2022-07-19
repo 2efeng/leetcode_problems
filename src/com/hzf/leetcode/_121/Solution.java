162b2f17c5eb73abb9f9dfc9c586b9787a729e50
h-2efeng <h-2efeng@qq.com>
2022-07-19 14:17:25 +0800

    leetcode 121.买卖股票的最佳时机

--- src/com/hzf/leetcode/_121/Solution.java
+++ src/com/hzf/leetcode/_121/Solution.java
@@ -5,18 +5,41 @@ package com.hzf.leetcode._121;
  * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
  * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
  * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ * <p>
+ * 提示：
+ * 1 <= prices.length <= 105
+ * 0 <= prices[i] <= 104
+ *
+ * <a href="https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/">...</a>
  */
 public class Solution {
 
-    /*
-      7,1,5,3,6,4
-      -7,-6,4,2,5,-2
-     */
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+
+    }
+
 
     public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+
+    //利用第53. 最大子数组和
+    public int maxProfit_053(int[] prices) {
         if (prices.length < 2) return 0;
         int maxProfit = 0;
-        int temp = 0;
+        int temp;
         int lastPrice = prices[0];
         prices[0] = prices[0] * -1;
         for (int i = 1; i < prices.length; i++) {
@@ -31,9 +54,8 @@ public class Solution {
         return maxProfit;
     }
 
-
-    //超时
-    public int maxProfit2(int[] prices) {
+    //数据多会超时
+    public int maxProfit_timeout(int[] prices) {
         int maxProfit = 0;
         for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i + 1; j < prices.length; j++) {
